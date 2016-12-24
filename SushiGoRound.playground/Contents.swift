2a93d7238461a5ac286e0033e9640dee8f37a175
jollyjoester <jollyjoester@gmail.com>
2016-12-25 03:23:33 +0900

    Add project

---
+++ SushiGoRound.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+let view = UIView(frame: CGRect(x: 0, y:0, width:320, height:44))
+view.backgroundColor = UIColor.black
+
+PlaygroundPage.current.liveView = view
+
+(0...2).forEach {
+    let sushi = UILabel(frame: CGRect(x:320, y:0, width:44, height:44))
+    sushi.text = "🍣"
+    
+    view.addSubview(sushi)
+    
+    UIView.animate(withDuration: 3, delay: TimeInterval($0), options: [.curveLinear, .repeat], animations: { () -> Void in
+        sushi.center = CGPoint(x: 0, y: sushi.center.y)
+        
+    }, completion: nil)
+}
---
+++ SushiGoRound/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SushiGoRound
+//
+//  Created by NanashimaHideyuki on 12/25/16.
+//  Copyright © 2016 jollyjoester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
